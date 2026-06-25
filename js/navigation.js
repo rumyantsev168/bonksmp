3f57e882bbb680591cfa92ef568710675c836db2
rumyantsev168 <Rmisha08len@yandex.ru>
2026-06-26 01:46:04 +0300

    minor adjustments

--- js/navigation.js
+++ js/navigation.js
@@ -1,19 +1,23 @@
 // adds the <nav> element
-const pagesFileNames = ["index", "rules", "updates", "socials", "cmds", "devs", "bans"];
-const pagesDisplayNames = ["home", "rules", "updates", "socials", "commands", "devs", "banlands"];
-let currentLink = "";
-let navMenu = "";
-pagesFileNames.forEach((name, i) => {
-    let pn = document.location.pathname.split("/").at(-1);
-    if (name == "index" && pn == "") {
-        currentLink = `<button disabled title="You are already here!">home</button>`;
-    } else if (pn == name+".html") {
-        currentLink = `<button disabled title="You are already here!">${pagesDisplayNames[i]}</button>`;
-    } else {
-        currentLink = `<a href="${name}.html"><button>${pagesDisplayNames[i]}</button></a>`;
-    };
-    navMenu = navMenu + currentLink;
+const links = [
+    { href: "/index", name: "home" },
+    { href: "/rules", name: "rules" },
+    { href: "/updates", name: "updates" },
+    { href: "/socials", name: "socials" },
+    { href: "/cmds", name: "commands" },
+    { href: "/devs", name: "devs" },
+    { href: "/bans", name: "banlands" }
+]
+const nav = document.createElement("nav");
+links.forEach(link => {
+    const navLink = document.createElement("a");
+    navLink.className = "nav";
+    navLink.href = link.href+".html";
+    navLink.innerText = link.name;
+    const pageName = `/${location.pathname.split("/").at(-1)}`;
+    if (pageName == link.href || pageName == link.href+".html") {
+        navLink.setAttribute("disabled", "");
+    }
+    nav.appendChild(navLink);
 });
-const navElement = document.createElement("nav");
-navElement.innerHTML = navMenu;
-document.body.prepend(navElement);
+document.body.prepend(nav);
