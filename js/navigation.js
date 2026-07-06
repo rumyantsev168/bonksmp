fd58cfcbd6a4f22eefc8b2b8d45f696a243f2aec
rumyantsev168 <Rmisha08len@yandex.ru>
2026-07-07 02:52:26 +0300

    fixed navigation links

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,21 @@
 // adds the <nav> element
 const links = [
-    { href: "/index", name: "home" },
-    { href: "/rules", name: "rules" },
-    { href: "/updates", name: "updates" },
-    { href: "/socials", name: "socials" },
-    { href: "/cmds", name: "commands" },
-    { href: "/devs", name: "devs" },
-    { href: "/bans", name: "banlands" }
+    { href: "index.html", name: "home" },
+    { href: "rules.html", name: "rules" },
+    { href: "updates.html", name: "updates" },
+    { href: "socials.html", name: "socials" },
+    { href: "cmds.html", name: "commands" },
+    { href: "devs.html", name: "devs" },
+    { href: "bans.html", name: "banlands" }
 ]
 const nav = document.createElement("nav");
 links.forEach(link => {
     const navLink = document.createElement("a");
     navLink.className = "nav";
-    navLink.href = link.href+".html";
+    navLink.href = link.href;
     navLink.innerText = link.name;
-    const pageName = `/${location.pathname.split("/").at(-1)}`;
-    if (pageName == link.href || pageName == link.href+".html") {
+    const pageName = location.pathname.split("/").at(-1);
+    if (pageName == link.href || (link.href == "index" && pageName == "")) {
         navLink.setAttribute("disabled", "");
     }
     nav.appendChild(navLink);
